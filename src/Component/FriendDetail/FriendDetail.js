cd205ae0fe116e31a2ae71204fd465d9a012f7ae
niazmurshedsiam <niazmurshed16@gmail.com>
2021-03-08 15:51:27 +0600

    create route parameter and read from route parameter multiple

--- src/Component/FriendDetail/FriendDetail.js
+++ src/Component/FriendDetail/FriendDetail.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
-const FriendDetail = () => {
+const FriendDetail = (props) => {
     const {friendId} = useParams();
+    const [friend,setFriend] = useState({});
+    
+    useEffect(()=>{
+        fetch(`https://jsonplaceholder.typicode.com/users/${friendId}`)
+        .then(res=>res.json())
+        .then(data=> setFriend(data))
+    },[])
     return (
         <div>
             <h1>This is Friend Detail Component : {friendId}</h1>
+            <p>{friend.name}</p>
+            {friend.email}
         </div>
     );
 };
